4c92dcd71f5b4dcc027838719edcd78a45c500dd
zgh <iuyo5678@gmail.com>
2013-11-29 15:20:28 +0800

    修复dired模式下一些问题

--- mylisps/dired-settings.el
+++ mylisps/dired-settings.el
@@ -1,20 +1,20 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2013-11-29 00:28:05 Friday by nilin>
+;; Time-stamp: <2013-11-29 11:53:31 Friday by zhangguhua>
 
 (require 'dired-single)
 (add-hook 'dired-load-hook
           (lambda ()
-            (define-key dired-mode-map (kbd "RET") 'joc-dired-single-buffer)
-            (define-key dired-mode-map (kbd "<mouse-1>") 'joc-dired-single-buffer-mouse)
+            (define-key dired-mode-map (kbd "RET") 'dired-single-buffer)
+            (define-key dired-mode-map (kbd "<mouse-1>") 'dired-single-buffer-mouse)
             (define-key dired-mode-map (kbd "^")
               (lambda ()
                 (interactive)
-                (joc-dired-single-buffer "..")))
-            (setq joc-dired-use-magic-buffer t)
-            (setq joc-dired-magic-buffer-name "*dired*")))
+                (dired-single-buffer "..")))
+            (setq dired-single-use-magic-buffer t)
+            (setq dired--single-magic-buffer-name "*dired*")))
 (global-set-key (kbd "C-x d")
-                'joc-dired-magic-buffer)
+                'dired-single-magic-buffer)
 
 (require 'dired-view)
 (define-key dired-mode-map (kbd ";") 'dired-view-minor-mode-toggle)
