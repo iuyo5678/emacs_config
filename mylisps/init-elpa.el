7455a8101f6b4b201a498820a547075844967a0b
zhangguhua <iuyo5678@gmail.com>
2016-02-26 16:33:54 +0800

    初始化elpa 添加相应的软件库

---
+++ mylisps/init-elpa.el
@@ -0,0 +1,73 @@
+;;; init-elpa.el --- 
+
+;; Copyright 2016 zhangguhua
+;;
+;; Author: zhangguhua@baidu.com
+;; Version: $Id: init-elpa.el,v 0.0 2016/02/25 12:05:58 zhangguhua Exp $
+;; Keywords: 
+;; X-URL: not distributed yet
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+;; 
+
+;; Put this file into your load-path and the following into your ~/.emacs:
+;;   (require 'init-elpa)
+
+;;; Code:
+
+
+;;;;##########################################################################
+;;;;  User Options, Variables
+;;;;##########################################################################
+
+(require 'package)
+(add-to-list 'package-archives 
+             '("marmalade" .
+               "http://marmalade-repo.org/packages/"))
+
+;; 增加软件包仓库
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(when (< emacs-major-version 24)
+    (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+;; 定义require-package函数
+(defun require-package (package &optional min-version no-refresh)
+    "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+    (if (package-installed-p package min-version)
+            t
+        (if (or (assoc package package-archive-contents) no-refresh)
+                (package-install package)
+            (progn
+                (package-refresh-contents)
+                (require-package package min-version t)))))
+;; 强行提前初始化ELPA。因为默认情况下Emacs在init.el加载完之后才开始初始化ELPA，
+;; 而我们把大多数包的初始化函数都放在init.el中，如果不提前初始化ELPA会导致后面的
+;; 初始化过程出错（对应的包文件还没有加载进来）。
+(package-initialize)
+
+
+(provide 'init-elpa)
+(eval-when-compile
+    (require 'cl))
+
+
+
+;;; init-elpa.el ends here
