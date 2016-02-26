426288c8c873a9b0f733ba83bca5540e87ed5583
zhangguhua <iuyo5678@gmail.com>
2016-02-26 21:49:21 +0800

    添加了一个个人配置的文件夹，lisps 保存自己的配置文件等不方便上传的信息。

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
 ;; -*- Emacs-Lisp -*-
-;; Time-stamp: <2016-02-26 18:09:42 Friday by zhangguhua>
+;; Time-stamp: <2016-02-26 21:47:36 Friday by zhangguhua>
 ;; zgh的emacs配置启动文件
 
 ;; 定义相关的路径，
 (defconst my-emacs-path           "~/.emacs.d/" "我的emacs相关配置文件的路径")
 (defconst my-emacs-my-lisps-path  (concat my-emacs-path "mylisps/") "我自己找的一些的emacs lisp包的路径")
 (defconst my-emacs-lisps-path     (concat my-emacs-path "elpa/") "一些安装lisp包路径，后面想用elpa升级")
+(defconst my-custom-config-path (concat my-emacs-path "lisps/") "一些自己自定义的配置信息，如自己的个人信息，个人博客配置等")
 (defconst my-emacs-templates-path (concat my-emacs-path "templates/") "模板路径")
 
 
@@ -13,6 +14,7 @@
 (load (concat my-emacs-my-lisps-path "my-subdirs"))
 (my-add-subdirs-to-load-path my-emacs-lisps-path)
 (my-add-subdirs-to-load-path my-emacs-my-lisps-path)
+(my-add-subdirs-to-load-path my-custom-config-path)
 
 ;;初始化配置elpa
 (require 'init-elpa)
