8fe532552d83699b4f55394b5ea5538f49998147
iuyo5678 <iuyo5678@gmail.com>
2016-03-15 15:35:43 +0800

    fix but

---
+++ elpa/python-environment-20150310.153/python-environment-autoloads.el
@@ -0,0 +1,16 @@
+;;; python-environment-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("python-environment-pkg.el" "python-environment.el"
+;;;;;;  "test-python-environment.el") (22247 47091 189541 543000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; python-environment-autoloads.el ends here
