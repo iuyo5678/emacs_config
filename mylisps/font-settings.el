ec7217534009a60672e303cfbc953f540892f6e7
zhangguhua <zhangguhua@localhost.localdomain>
2016-07-01 22:03:59 +0800

    对模板文件进行了一些更改

--- mylisps/font-settings.el
+++ mylisps/font-settings.el
@@ -1,14 +1,11 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2016-06-12 17:20:57 Sunday by zhangguhua>
+;; Time-stamp: <2016-07-01 19:30:14 Friday by zhangguhua>
 
 (when (and window-system is-after-emacs-23)
   (require 'my-fontset-win)
   (if mswin
-      ;; (huangq-fontset-courier 17)
       (huangq-fontset-consolas 14)
-    ;; (huangq-fontset-dejavu 17)))
-    ;; (huangq-fontset-fixedsys 17)
-    (set-default-font "Monospace 13")))
+      (set-default-font "Monospace 15")))  ;;修改emacs字体大小
 
 (provide 'font-settings)
