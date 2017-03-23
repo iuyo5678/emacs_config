60b74d2268bbd128bae50a68b59008308a45c9fc
Zhang Guhua <iuyo5678@gmail.com>
2017-03-23 10:31:21 +0800

    升级插件文件

--- elpa/pyvenv-1.9/pyvenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pyvenv" "1.9" "Python virtual environment interface" 'nil :url "http://github.com/jorgenschaefer/pyvenv" :keywords '("python" "virtualenv" "tools"))
---
+++ elpa/slime-20100404.1/slime-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "slime" "20100404.1" "Superior Lisp Interaction Mode for Emacs" 'nil)
