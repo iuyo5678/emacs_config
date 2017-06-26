d063d78341f558831f341742ab001bcffdd3b0c3
Zhang Guhua <iuyo5678@gmail.com>
2017-06-26 12:01:47 +0800

    update monthly

---
+++ elpa/ein-0.12.8/ein-pkg.el
@@ -0,0 +1,9 @@
+(define-package "ein" "0.12.8" "Emacs IPython Notebook"
+  '((websocket "1.6")
+    (request "0.3")
+    (deferred "0.3")
+    (cl-generic "0.3")
+    (dash "2.13.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
