c7c261f096f65374fe9ceaf0af24d4faba8c6119
drakezhang <iuyo5678@gmail.com>
2018-03-07 08:58:26 +0800

    update the color theme config

--- mylisps/face-settings.el
+++ mylisps/face-settings.el
@@ -1,12 +1,12 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2018-02-06 19:02:23 Tuesday by zhangguhua>
+;; Time-stamp: <2018-02-06 19:12:57 Tuesday by zhangguhua>
 ;;主题配置
 (require 'color-theme)
 (color-theme-initialize)
 
+(require 'zenburn)
 (require 'ahei-face)
 (require 'color-theme-ahei)
 (require 'tomorrow-night-theme)
-(color-theme-tomorrow-night)
 (provide 'face-settings)
