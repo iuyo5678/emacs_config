6970e5401522571fa73ae29b736fb60d0f08c012
drakezhang <drakezhang@tencent.com>
2021-05-12 12:25:49 +0800

    修改默认字体和字体的大小

--- mylisps/face-settings.el
+++ mylisps/face-settings.el
@@ -4,6 +4,9 @@
 ;;主题配置
 
 ;; Font
+
+(set-frame-font "Monaco")
+(face-spec-set 'default `((t (:height , 180))))
 (defun font-installed-p (font-name)
   "Check if font with FONT-NAME is available."
   (find-font (font-spec :name font-name)))
